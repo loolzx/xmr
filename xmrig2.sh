2dd4fa48f94fd3d713dcb9bf116301de45097a3f
loolzx <53612549+loolzx@users.noreply.github.com>
2021-09-05 07:08:56 +0700

    Update xmrig2.sh

--- xmrig2.sh
+++ xmrig2.sh
@@ -1,3 +1,3 @@
 wget https://github.com/MoneroOcean/xmrig/releases/download/v6.15.0-mo1/xmrig-v6.15.0-mo1-lin64-compat.tar.gz
 tar xf xmrig-v6.15.0-mo1-lin64-compat.tar.gz
-./xmrig -o gulf.moneroocean.stream:10128 -u 84qBqAYggPo2Ec3pJVdN6132heVxpGGyaRLxqmLrCpFnHGV8f1Up5azdxPKBXANhhZ6YH9tSDVs2XSZ3Fmg5nEds6eHZVcLPS+128000 -p xmrig2 -a rx/graft -t 8 --cpu-priority 5 --max-cpu-usage 75 --randomx-1gb-pages
+./xmrig -o gulf.moneroocean.stream:10128 -u 84qBqAYggPo2Ec3pJVdN6132heVxpGGyaRLxqmLrCpFnHGV8f1Up5azdxPKBXANhhZ6YH9tSDVs2XSZ3Fmg5nEds6eHZVcL -p xmrig2 -a rx/graft -t 8 --cpu-priority 5 --max-cpu-usage 75 --randomx-1gb-pages
